c73c1f1c8b11d91894017485ee6d7768d5ce21ff
robynetzan <dragonfree97@live.co.uk>
2023-03-16 12:22:26 +0000

    now it works

--- converter.js
+++ converter.js
@@ -2,3 +2,44 @@ const fs = require("fs");
 const csv=require('csvtojson');
 const csvFilePath='./otd.csv';
 
+var main = async function(){
+	
+var otds = await csv()
+	.fromFile(csvFilePath)
+	.then((jsonObj)=>{
+		return(jsonObj);
+	});
+	
+	console.log(otds);
+	
+	var newOtds = {};
+	
+	for (var d in otds) {
+		
+		var date = otds[d].date;
+		
+		var year = date.slice(0,4);
+		var month = date.slice(5,7);
+		var day = date.slice(8,10);
+		
+		if (!newOtds.hasOwnProperty(year)) {
+			newOtds[year] = {};
+		}
+		if (!newOtds[year].hasOwnProperty(month)) {
+			newOtds[year][month] = {};
+		}
+		if (!newOtds[year][month].hasOwnProperty(day)) {
+			newOtds[year][month][day] = [];
+		}
+		
+		newOtds[year][month][day].push(otds[d].item);
+		
+	}
+	
+	
+	
+	console.log(newOtds[2018][10]);
+	
+	fs.writeFileSync("otds.json", JSON.stringify(newOtds, null, 2));
+	
+}();
